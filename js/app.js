5e22011704e08bfaad0ca8a08f68b6c82251e18f
Silverwolfceh <silverwolf@ceh.vn>
2021-10-18 19:47:19 +0700

    Tooo lazy

--- js/app.js
+++ js/app.js
@@ -69,3 +69,18 @@ anime.timeline({loop: true})
       return 100 + 30 * i;
     }
   });
+
+  function load_script_no_cache(url) {
+    url = url + "?v=" + get_random(1,1000000);
+    return new Promise(function(resolve, reject) {
+        var script = document.createElement("script");
+        script.onload = resolve;
+        script.onerror = reject;
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
+}
+
+function get_random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
